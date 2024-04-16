6d27c0abec5fc035831b639493d475685f1157f8
abhi1geeks <alan.alex.12797@gmail.com>
2024-04-16 09:23:10 +0530

    Added a new function to the core package

--- pypacter-api/src/pypacter_api/base.py
+++ pypacter-api/src/pypacter_api/base.py
@@ -8,9 +8,27 @@ version information.
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
+from pypacter.programming_lang_detector import detect_language
 from pypacter_api import get_version
 
 router = APIRouter()
+
+
+@router.post("/detect-programming-language", tags=["Language"])
+async def detect_programming_language_api(code_snippet: str) -> JSONResponse:
+    """
+    Endpoint to detect the language of a code snippet.
+
+    Accepts a POST request with a text payload containing the code snippet.
+
+    Args:
+    code_snippet: The code snippet to analyze.
+
+    Returns:
+    A JSON response containing the detected language.
+    """
+    language = detect_language(code_snippet)
+    return JSONResponse(content={"language": language})
 
 
 @router.get("/health", tags=["health"])
